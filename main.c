8a20f9ad39b079e710a956f6a910865f465dc2c3
austen barker <austenbarker@yahoo.com>
2018-02-07 19:56:01 -0800

    should build

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdio.h>
 
+#include "character.h"
+#include "dice.h"
+
+extern char **lexgetline();
 
 int main(){
+	int i;
+	char **args;
+	while(1){
+		args = lexgetline();
+		for(i=0; args[i] != NULL; i++){
+
+		}
+	}
 	return 0;
 }
